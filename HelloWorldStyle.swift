375f9e822c3932c64d7bd82a2907d55528320c32
Jack Turcotte <jack.turcotte@stu.ocsb.ca>
2025-09-10 00:18:15 +0000

    Created hello world style

---
+++ HelloWorldStyle.swift
@@ -0,0 +1,12 @@
+//
+//  HelloWorld.swift
+//
+//  Created by Jack Turcotte
+//  Created on 2025-09-05
+//  Version 1.0
+//  Copyright (c) 2025 Jack Turcotte. All rights reserved.
+//
+
+//  This prints hello world in swift
+
+print("Hello world!")
